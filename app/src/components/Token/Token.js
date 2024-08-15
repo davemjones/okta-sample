0d8f2d2752af323f053c94e92fa627a09bf6d649
David Jones <davemjones@gmail.com>
2024-08-15 22:52:05 +0000

    added token table

--- app/src/components/Token/Token.js
+++ app/src/components/Token/Token.js
@@ -1,43 +1,61 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { checkAndRefreshToken } from "../../utils/tokenUtils";
 import oktaAuth from "../oktaAuth";
+import "./token.css";
+import TokenDetail from "./components/TokenDetail";
 
 const Token = () => {
-  let token = {};
+  const [token, setToken] = useState(undefined);
+  const [oldToken, setOldToken] = useState(undefined);
 
   const checkToken = async () => {
     // check only, do not refresh
-    token = await checkAndRefreshToken(false);
-    if (!token) {
-      console.log("User is not authenticated. Redirecting to login...");
-      oktaAuth.signInWithRedirect();
-    } else {
-      console.log("current token:");
-      console.table(token);
-    }
+    await checkAndRefreshToken(false).then((currentToken) => {
+      setToken(currentToken);
+      if (!currentToken) {
+        console.log("User is not authenticated. Redirecting to login...");
+        oktaAuth.signInWithRedirect();
+      } else {
+        console.log("current token:");
+        console.table(currentToken);
+      }
+    });
   };
 
   const refreshToken = async () => {
     // check only, do not refresh
-    token = await checkAndRefreshToken(true);
-    if (!token) {
-      console.log("User is not authenticated. Redirecting to login...");
-      oktaAuth.signInWithRedirect();
-    } else {
-      console.log("refreshed token:");
-      console.table(token);
-    }
+    await checkAndRefreshToken(true).then((newToken) => {
+      setOldToken(token);
+      setToken(newToken);
+      if (!newToken) {
+        console.log("User is not authenticated. Redirecting to login...");
+        oktaAuth.signInWithRedirect();
+      } else {
+        console.log("refreshed token:");
+        console.table(newToken);
+      }
+    });
   };
 
   useEffect(() => {
     checkToken(false);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <section>
+    <section className="token-component">
       <h2>Token</h2>
-      <div>
+      <div className="token-component__details">
+        <details className="token-component__details-current">
+          <summary>Current Token</summary>
+          <TokenDetail token={token} />
+        </details>
+        <details className="token-component__details-old">
+          <summary>Old Token</summary>
+          <TokenDetail token={oldToken} />
+        </details>
+      </div>
+      <div className="token-component__actions">
         <button onClick={checkToken}>Check Token</button>
         <button onClick={refreshToken}>Refresh Token</button>
       </div>
---
+++ app/src/components/Token/components/TokenDetail.js
@@ -0,0 +1,106 @@
+import React from "react";
+import "./token-detail.css";
+
+const convertUnixToFormattedTimestamp = (unixTimestamp) =>
+  new Date(unixTimestamp * 1000).toLocaleDateString("en-US") +
+  " " +
+  new Date(unixTimestamp * 1000).toLocaleTimeString("en-US");
+
+const Item = ({ name, value, tip }) => {
+  return (
+    <>
+      <div className="token-detail__name">{name}</div>
+      <div className="token-detail__value">{value}</div>
+      {/* <div role="tooltip" id={name}>{tip}</div> */}
+    </>
+  );
+};
+
+const TokenDetail = ({ token }) => {
+  const details = [
+    {
+      name: "accessToken",
+      token: token?.accessToken,
+    },
+    {
+      name: "authorizeUrl",
+      token: token?.authorizeUrl,
+    },
+    {
+      name: "expiresAt",
+      token: `${token?.expiresAt} (${convertUnixToFormattedTimestamp(
+        token?.expiresAt
+      )})`,
+    },
+    {
+      name: "tokenType",
+      token: token?.tokenType,
+    },
+    {
+      name: "userinfoUrl",
+      token: token?.userinfoUrl,
+    },
+    {
+      name: "claims: aud",
+      token: token?.claims?.aud,
+    },
+    {
+      name: "claims: auth_time",
+      token: `${token?.claims?.auth_time} (${convertUnixToFormattedTimestamp(
+        token?.claims?.auth_time
+      )})`,
+    },
+    {
+      name: "claims: cid",
+      token: token?.claims?.cid,
+    },
+    {
+      name: "claims: exp",
+      token: `${token?.claims?.exp} (${convertUnixToFormattedTimestamp(
+        token?.claims?.exp
+      )})`,
+    },
+    {
+      name: "claims: iat",
+      token: `${token?.claims?.iat} (${convertUnixToFormattedTimestamp(
+        token?.claims?.iat
+      )})`,
+    },
+    {
+      name: "claims: iss",
+      token: token?.claims?.iss,
+    },
+    {
+      name: "claims: jti",
+      token: token?.claims?.jti,
+    },
+    {
+      name: "claims: scp",
+      token: Array.isArray(token?.claims?.scp)
+        ? `[${token?.claims?.scp.join(", ")}]`
+        : "[]",
+    },
+    {
+      name: "claims: uid",
+      token: token?.claims?.uid,
+    },
+    {
+      name: "claims: ver",
+      token: token?.claims?.ver,
+    },
+  ];
+
+  return (
+    <section className="token-detail">
+      {token && (
+        <div className="token-detail__container">
+          {details.map((item) => (
+            <Item key={item.name} name={item.name} value={item.token} />
+          ))}
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default TokenDetail;
